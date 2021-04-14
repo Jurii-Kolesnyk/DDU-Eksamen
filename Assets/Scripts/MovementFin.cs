ae40e9d9c026483c713bfcba50e0052b8b7aebab
Tobias <tobiasbjerregaard@live.dk>
2021-04-14 14:13:00 +0200

    Uploaded kode til movement

--- Assets/Scripts/MovementFin.cs
+++ Assets/Scripts/MovementFin.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MovementFin : MonoBehaviour
 {
+
+    public float speed;
+    private Vector2 change;
+    private Rigidbody2D myRigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        myRigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // For hvert frame resetter jeg hvor meget player har ændret sig
+        change = Vector2.zero;
+
+        // Man bruger GetAxisRaw for at undgå acceleration eller modstands acceleration som gør movement kommer til føle mere "snappy"
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
     }
 }
