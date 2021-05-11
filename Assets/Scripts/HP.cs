3ad27e35508d3d582a99ebca56fe3d8393303d43
Granada <alexandermadsen04@gmail.com>
2021-05-11 15:10:50 +0200

    hp bar

---
+++ Assets/Scripts/HP.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+   public int health;
+   public int numOfHearts;
+   public Image[] hearts;
+   public Sprite fullHeart;
+   public Sprite emptyHeart;
+}
